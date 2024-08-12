3b4df1d7ce5c95856ba000d635f49fb19a65af43
Antonio <antoniocarmagnani@hotmail.com>
2024-08-12 16:58:01 -0300

    remove iframe and add app link

--- links.js
+++ links.js
@@ -1,7 +1,6 @@
-// links.js
 const links = {
-    plataform: "https://go.aff.elisa.bet/pbs688xc",
-    kirvano: "https://app.digitalmartbr.com.br/checkout/vbiiuayj",
+    plataform: "https://elisahack.com/hackninja/user/fast-login",
+    kirvano: "#",
     menu1: "#",
     menu2: "#",
     menu3: "#",
@@ -21,7 +20,15 @@ function updateElements() {
             }
         }
     }
-    
+
+    // Update the button to open the link in a new tab
+    const button = document.getElementById('link-plataforma');
+    if (button) {
+        button.onclick = function() {
+            window.open(links.plataform, '_blank');
+        };
+    }
+
     const footer = document.getElementById('footer');
     if (footer) {
         footer.innerHTML = links.footerText;
